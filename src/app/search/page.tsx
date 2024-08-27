ac9817266bd53fbfc06d2f04cf4402fcede786da
nas <y.softvalley@hey.com>
2024-08-27 12:06:07 +0900

    検索入力フィールドのスタイルを変更
    * ユーザーインターフェースの一貫性を保つために、検索入力フィールドのフォーカス時のリング表示を削除
    * フォーカスを明確に示すために、必要なスタイルのみを保持することで視覚的に整理された印象を与える
    * 今後のデザイン変更やスタイル調整を容易にするため、余分なクラスを削除した

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -50,7 +50,7 @@ const SearchBox = connectSearchBox(
           </div>
           <input
             ref={inputRef}
-            className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-gray-400 focus:border-gray-400 sm:text-sm"
+            className="block w-full pl-10 pr-3 py-2 border border-gray-300 leading-5 bg-white focus:outline-none focus:ring-0 focus:ring-none sm:text-sm"
             value={currentRefinement}
             type="text"
             placeholder=""
