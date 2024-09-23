11c0b71e1e261beff94b7c699feaa2be9d936143
nas <y.softvalley@hey.com>
2024-09-24 02:42:38 +0900

    ドラフトコンテンツの表示条件を環境変数で制御できるように変更
    * 開発環境でのドラフトコンテンツの管理を容易にするため
    * `NODE_ENV` に依存せず、環境変数 `NEXT_PUBLIC_CONTAINS_DRAFT` でドラフトの可視性を制御することで、環境に応じた柔軟なコンテンツ表示が可能となる
    * これにより、開発者は簡単にドラフトを確認しながら作業できるようになる

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -4,7 +4,7 @@ import matter from "gray-matter";
 import path from "path";
 import { genMetmuseumObjectId, getImageData } from "./metmuseum";
 
-const prod = process.env.NODE_ENV === "production";
+const containsDraft = process.env.NEXT_PUBLIC_CONTAINS_DRAFT === "true";
 
 const contentsDirectory = path.join(process.cwd(), "contents");
 
@@ -61,7 +61,7 @@ export const getAllContents = async (): Promise<Content[]> => {
     await Promise.all(slugs.map((slug) => getContentBySlug(slug)))
   )
     .filter((content): content is Content => content !== null)
-    .filter((content) => !prod || content.status === "published")
+    .filter((content) => containsDraft || content.status === "published")
     .sort((content1, content2) =>
       content1.datetime > content2.datetime ? -1 : 1,
     );
